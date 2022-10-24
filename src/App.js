7d980380ea390894bdd0117a28d106d3adf052d3
Franziska S <fra.se@posteo.net>
2022-10-24 21:44:15 +0200

    style changes

--- src/App.js
+++ src/App.js
@@ -1,79 +1,89 @@
-import { useEffect, useRef, useState } from "react";
-import FlashcardList from "./flashcards/FlashcardList";
-import "./App.css"
-import axios from "axios";
-
+import { useEffect, useRef, useState } from 'react';
+import FlashcardList from './flashcards/FlashcardList';
+import './App.css';
+import axios from 'axios';
 
 function App() {
-  const [flashcards, setFlashcards] = useState([])
-  const [categories, setCategories] = useState([])
-  const categoryEl = useRef()
-  const amountEl = useRef()
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const categoryEl = useRef();
+  const amountEl = useRef();
 
   useEffect(() => {
-    axios
-      .get("https://opentdb.com/api_category.php")
-      .then(res => {
-        setCategories(res.data.trivia_categories)
-      })
-  }, [])
+    axios.get('https://opentdb.com/api_category.php').then((res) => {
+      setCategories(res.data.trivia_categories);
+    });
+  }, []);
 
-  //decodes HTML encoded stuff 
+  //decodes HTML encoded stuff
   function decodeString(str) {
-    const textArea = document.createElement("textarea")
-    textArea.innerHTML = str
-    return textArea.value
+    const textArea = document.createElement('textarea');
+    textArea.innerHTML = str;
+    return textArea.value;
   }
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     axios
-      .get("https://opentdb.com/api.php?", {
+      .get('https://opentdb.com/api.php?', {
         params: {
           amount: amountEl.current.value,
-          category: categoryEl.current.value
-        }
+          category: categoryEl.current.value,
+        },
       })
-      .then(res => {
-        setFlashcards(res.data.results.map((questionItem, index) => {
-          const answer = decodeString(questionItem.correct_answer)
-          const options = [
-            ...questionItem.incorrect_answers.map(a => decodeString(a)),
-            answer]
-          return {
-            id: `${index} - ${Date.now()}`,
-            question: decodeString(questionItem.question),
-            answer: answer,
-            options: options.sort(() => Math.random() - .5) //randomly 50% positive/negative number
-          }
-        }))
-      })
-  }
+      .then((res) => {
+        setFlashcards(
+          res.data.results.map((questionItem, index) => {
+            const answer = decodeString(questionItem.correct_answer);
+            const options = [
+              ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+              answer,
+            ];
+            return {
+              id: `${index} - ${Date.now()}`,
+              question: decodeString(questionItem.question),
+              answer: answer,
+              options: options.sort(() => Math.random() - 0.5), //randomly 50% positive/negative number
+            };
+          })
+        );
+      });
+  };
 
   return (
-    <>
-      <form className="header" onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="category">Category</label>
-          <select id="category" ref={categoryEl}>
+    <div className='wrapper'>
+      <form className='header' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='category'>Category</label>
+          <select id='category' ref={categoryEl}>
             {categories.map((category) => {
-              return <option value={category.id} key={category.id}>{category.name}</option>
+              return (
+                <option value={category.id} key={category.id}>
+                  {category.name}
+                </option>
+              );
             })}
           </select>
         </div>
-        <div className="form-group">
-          <label htmlFor="amount">Number of Questions</label>
-          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl}></input>
+        <div className='form-group'>
+          <label htmlFor='amount'>Number of Questions</label>
+          <input
+            type='number'
+            id='amount'
+            min='1'
+            step='1'
+            defaultValue={10}
+            ref={amountEl}
+          ></input>
         </div>
-        <div className="form-group">
-          <button className="btn">Generate</button>
+        <div className='form-group'>
+          <button className='btn'>Generate</button>
         </div>
       </form>
-      <div className="container">
+      <div className='container'>
         <FlashcardList flashcards={flashcards} />
       </div>
-    </>
+    </div>
   );
 }
-
 
 export default App;
